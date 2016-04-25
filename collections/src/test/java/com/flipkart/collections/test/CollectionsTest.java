c74cfb613a747eb0e0de80246d723c93b8d71481
durga.p <durga.p@flipkart.com>
2016-04-25 11:44:34 +0530

    Custom sort

---
+++ collections/src/test/java/com/flipkart/collections/test/CollectionsTest.java
@@ -0,0 +1,65 @@
+package com.flipkart.collections.test;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CollectionsTest {
+
+  @Test
+  public void iteration() {
+    Collection<Integer> integers = Arrays.asList(1,2,3,4,5);
+
+    Iterator<Integer> iterator = integers.iterator();
+
+    /*
+    iterator.hasNext();
+    iterator.next();
+    iterator.remove();
+    */
+    while (iterator.hasNext()) {
+      System.out.println("Element value using iterator: " + iterator.next());
+    }
+
+    for (Integer integer : integers) {
+      System.out.println("Element value using forloop : " + integer);
+    }
+
+    assertEquals(true, true);
+  }
+
+  @Test
+  public void sort() {
+    List<Integer> integers = Arrays.asList(4,1,5,3,2);
+
+    Collections.sort(integers);
+    assertEquals(integers, Arrays.asList(1,2,3,4,5));
+
+    Collections.sort(integers, Collections.reverseOrder());
+    assertEquals(integers, Arrays.asList(5,4,3,2,1));
+
+    Collections.sort(integers, new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        return 0;
+      }
+    });
+    assertEquals(integers, Arrays.asList(2,4,1,3,5));
+
+
+
+  }
+
+
+
+
+
+
+}
