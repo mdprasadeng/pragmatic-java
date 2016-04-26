c2832ff39fefcf438b06392b38862bbc748d87d1
durga.p <durga.p@flipkart.com>
2016-04-26 10:24:40 +0530

    sample collectors

--- stream/src/test/java/com/flipkart/stream/test/CollectorsTest.java
+++ stream/src/test/java/com/flipkart/stream/test/CollectorsTest.java
@@ -1,5 +1,19 @@
 package com.flipkart.stream.test;
 
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
 public class CollectorsTest {
 
+  @Test
+  public void test() {
+
+    Stream.of(new String[]{"This", "is", "a", "sample", "sentence"});
+        //.collect(Collectors.joining(" "))
+        //.collect(Collectors.toList())
+        //.collect(Collectors.toMap(e -> e, e -> e.length()));
+
+
+  }
 }
